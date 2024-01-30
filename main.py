6a2c76ea2250899f1af46da64e372d13459695b1
rajsarky <k.n.sarkar@icloud.com>
2024-01-30 17:49:25 +0000

    create three segment - starting snake

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
-seg_1 = turtle.Turtle("square")
-seg_1.color("white")
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
+
+for position in starting_position:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
 
 
