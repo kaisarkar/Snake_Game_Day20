faf79c11457b3a071045d3a9d5eea2a07997586f
rajsarky <k.n.sarkar@icloud.com>
2024-01-30 21:40:05 +0000

    slicing

--- main.py
+++ main.py
@@ -26,11 +26,13 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
     # Detect collision with food
     if snake.head.distance(food) < 15:
         snake.extend()
         scoreboard.increase_score()
         food.refresh()
+
     # Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         scoreboard.game_over()
