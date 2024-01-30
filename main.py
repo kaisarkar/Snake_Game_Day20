a77115c522ae8d66585775b7878533048cc0a925
rajsarky <k.n.sarkar@icloud.com>
2024-01-30 17:46:12 +0000

    Creating white box

---
+++ main.py
@@ -0,0 +1,23 @@
+import turtle
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+seg_1 = turtle.Turtle("square")
+seg_1.color("white")
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
